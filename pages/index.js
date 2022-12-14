d6b429cc2198924a952df0f0a4e115c403f437b9
Helder Fonseca <helder2391@gmail.com>
2022-12-14 14:51:41 -0100

    add about info and footer

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,21 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Lead from '../components/Lead'
 import PacoteList from '../components/PacoteList'
+import About from '../components/About'
+import Footer from '../components/Footer'
 import Testimonials from '../components/Testimonial'
 
 export default function Home() {
   return (
-    <div className='pb-10'>
+    <div className=''>
         <div className="pt-8 md:h-screen px-10 bg-no-repeat bg-cover bg-center bg-[url('/background-hero-mobile.png')] xl:bg-[url('/background-hero.png')]">
           <Header />
           <Hero />
         </div>
+        <About />
         <PacoteList />
         <ContactForm />
+        <Footer />
         {/*<Lead />
         <Testimonials />*/}
     </div>
